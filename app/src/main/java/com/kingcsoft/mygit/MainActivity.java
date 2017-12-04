aeb4eb3d61a2bb8f95855e75052152914a7c3f57
kingcxsc <34228888+kingcxsc@users.noreply.github.com>
2017-12-04 19:10:45 +0800

    Update MainActivity.java

--- app/src/main/java/com/kingcsoft/mygit/MainActivity.java
+++ app/src/main/java/com/kingcsoft/mygit/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Log.e("111111111","1111111111");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
